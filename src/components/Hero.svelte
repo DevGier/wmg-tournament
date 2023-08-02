2bfd62a12ee0c0074e7537d088a5b271f68e75ec
Tim de Gier <timdegier@MBP-Tim.local>
2023-08-02 17:09:20 +0200

    Fixed hero

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,6 +1,6 @@
-<div class="rounded-xl relative flex items-end w-full p-20 overflow-hidden shadow-xl">
+<div class="rounded-xl md:p-20 relative flex items-end w-full p-8 overflow-hidden shadow-xl">
 	<img
-		class="absolute inset-0 object-center"
+		class="absolute inset-0 object-center w-full h-full"
 		src="https://images.squarespace-cdn.com/content/v1/5f526bb40c688205a903d329/1646688700787-XM4RVYDS0F291T0T9F83/WalkaboutMiniGolf-TouristTrap-Night19.png"
 		alt="Tourist trap hard"
 	/>
